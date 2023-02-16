cfcb1fd23ddb8e7657b6527ac2f98a1b93b03e4a
Tito Parra <tito.parra4@hotmail.com>
2023-02-15 21:33:16 -0500

    Project Architecture

---
+++ Wevpshop.mobile/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace Wevpshop.mobile
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ Wevpshop.mobile/Platforms/iOS/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace Wevpshop.mobile
+{
+    [Register("AppDelegate")]
+    public class AppDelegate : MauiUIApplicationDelegate
+    {
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    }
+}
