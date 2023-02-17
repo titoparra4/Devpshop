b41c209b348d2b781f3469109e90f715cb19c323
Tito Parra <tito.parra4@hotmail.com>
2023-02-16 20:14:06 -0500

    Create DataBase

---
+++ Devpshop.API/Data/DataContext.cs
@@ -0,0 +1,21 @@
+﻿using Devpshop.Shared.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace Devpshop.API.Data
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions<DataContext> options) : base(options)
+        {
+
+        }
+
+        public DbSet<Country> Countries { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+            modelBuilder.Entity<Country>().HasIndex(x => x.Name).IsUnique();
+        }
+    }
+}
