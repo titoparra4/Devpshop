10faceed5be9e1ced12dbf48e8699f3fcfa07859
Tito Parra <tito.parra4@hotmail.com>
2023-04-17 20:50:16 -0500

    Add mobile project

---
+++ Devpshop.Mobile/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace Devpshop.Mobile
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
