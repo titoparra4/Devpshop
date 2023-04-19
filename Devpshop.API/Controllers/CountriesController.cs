b6f334caf137ff4b9c0ad3a14dc1b3fe8e43ae08
Tito Parra <tito.parra4@hotmail.com>
2023-04-18 20:54:32 -0500

    Countries API Controller

---
+++ Devpshop.API/Controllers/CountriesController.cs
@@ -0,0 +1,35 @@
+﻿using Devpshop.API.Data;
+using Devpshop.Shared.Entities;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace Devpshop.API.Controllers
+{
+    [ApiController]
+    [Route("/api/countries")]
+    public class CountriesController: ControllerBase
+    {
+        private readonly DataContext _context;
+
+        public CountriesController(DataContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetAsync() 
+        {
+            return Ok(await _context.Countries.ToListAsync());
+        }
+
+        [HttpPost]
+        public async Task<ActionResult> PostAsync(Country country)
+        {
+            _context.Add(country);
+            await _context.SaveChangesAsync();
+            return Ok(country);
+        }
+    }
+
+
+}
