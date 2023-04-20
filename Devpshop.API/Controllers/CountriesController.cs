6aabcff22aa344e39c60d4db0d074802bd8dae09
Tito Parra <tito.parra4@hotmail.com>
2023-04-19 22:08:20 -0500

    Complete countries API Controller

--- Devpshop.API/Controllers/CountriesController.cs
+++ Devpshop.API/Controllers/CountriesController.cs
@@ -22,14 +22,47 @@ namespace Devpshop.API.Controllers
             return Ok(await _context.Countries.ToListAsync());
         }
 
-        [HttpPost]
+		[HttpGet("{id:int}")]
+		public async Task<IActionResult> GetAsync(int id)
+		{
+            var country = await _context.Countries.FirstOrDefaultAsync(x => x.Id == id);
+            if(country == null)
+            {
+                return NotFound();
+            }
+			return Ok(country);
+		}
+
+		[HttpPost]
         public async Task<ActionResult> PostAsync(Country country)
         {
             _context.Add(country);
             await _context.SaveChangesAsync();
             return Ok(country);
         }
-    }
+
+		[HttpPut]
+		public async Task<ActionResult> PtAsync(Country country)
+		{
+			_context.Update(country);
+			await _context.SaveChangesAsync();
+			return Ok(country);
+		}
+
+		[HttpDelete("{id:int}")]
+		public async Task<IActionResult> DeleteAsync(int id)
+		{
+			var country = await _context.Countries.FirstOrDefaultAsync(x => x.Id == id);
+			if (country == null)
+			{
+				return NotFound();
+			}
+
+			_context.Remove(country);
+			await _context.SaveChangesAsync();
+			return NoContent();
+		}
+	}
 
 
 }
