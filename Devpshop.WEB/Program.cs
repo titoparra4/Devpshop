8e7eb590a267deac667725f810fc55668ee7a87e
Tito Parra <tito.parra4@hotmail.com>
2023-04-19 20:48:45 -0500

    Create countries page

--- Devpshop.WEB/Program.cs
+++ Devpshop.WEB/Program.cs
@@ -1,4 +1,5 @@
 using Devpshop.WEB;
+using Devpshop.WEB.Repositories;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 
@@ -6,6 +7,7 @@ var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
-builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri("https://localhost:7162/") });
+builder.Services.AddScoped<IRepository, Repository>();
 
 await builder.Build().RunAsync();
---
+++ Devpshop.WEB/Repositories/IRepository.cs
@@ -0,0 +1,12 @@
+﻿namespace Devpshop.WEB.Repositories
+{
+    public interface IRepository
+    {
+        Task<HttpResponseWrapper<T>> Get<T>(string url);
+
+        Task<HttpResponseWrapper<object>> Post<T>(string url, T model);
+
+        Task<HttpResponseWrapper<TResponse>> Post<T, TResponse>(string url, T model);
+    }
+
+}
