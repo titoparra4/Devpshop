cbbc61d23800d255e44c3f10046d3c568080ab31
Tito Parra <tito.parra4@hotmail.com>
2023-04-21 22:20:57 -0500

    Fix countries CRUD

--- Devpshop.API/Controllers/CountriesController.cs
+++ Devpshop.API/Controllers/CountriesController.cs
@@ -1,4 +1,5 @@
-﻿using Devpshop.API.Data;
+﻿using Azure.Core;
+using Devpshop.API.Data;
 using Devpshop.Shared.Entities;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
@@ -36,18 +37,50 @@ namespace Devpshop.API.Controllers
 		[HttpPost]
         public async Task<ActionResult> PostAsync(Country country)
         {
-            _context.Add(country);
-            await _context.SaveChangesAsync();
-            return Ok(country);
+			try
+			{
+				_context.Add(country);
+				await _context.SaveChangesAsync();
+				return Ok(country);
+			}
+			catch (DbUpdateException dbUpdateException)
+			{
+				if(dbUpdateException.InnerException!.Message.Contains("duplicate"))
+				{
+					return BadRequest("Ya existe un país con el mismo nombre.");
+				}
+
+				return BadRequest(dbUpdateException.Message);
+			}
+			catch(Exception exception) 
+			{
+                return BadRequest(exception.Message);
+            }
         }
 
 		[HttpPut]
-		public async Task<ActionResult> PtAsync(Country country)
+		public async Task<ActionResult> PutAsync(Country country)
 		{
-			_context.Update(country);
-			await _context.SaveChangesAsync();
-			return Ok(country);
-		}
+			try
+			{
+				_context.Update(country);
+				await _context.SaveChangesAsync();
+				return Ok(country);
+			}
+            catch (DbUpdateException dbUpdateException)
+            {
+                if (dbUpdateException.InnerException!.Message.Contains("duplicate"))
+                {
+                    return BadRequest("Ya existe un país con el mismo nombre.");
+                }
+
+                return BadRequest(dbUpdateException.Message);
+            }
+            catch (Exception exception)
+            {
+                return BadRequest(exception.Message);
+            }
+        }
 
 		[HttpDelete("{id:int}")]
 		public async Task<IActionResult> DeleteAsync(int id)
