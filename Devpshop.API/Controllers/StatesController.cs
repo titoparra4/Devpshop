9d7f99267b5b3a37bfa74de9871bee4819265712
Tito Parra <tito.parra4@hotmail.com>
2023-04-28 22:06:24 -0500

    Create Page States

--- Devpshop.API/Controllers/StatesController.cs
+++ Devpshop.API/Controllers/StatesController.cs
@@ -29,7 +29,9 @@ namespace Devpshop.API.Controllers
 		[HttpGet("{id:int}")]
 		public async Task<IActionResult> GetAsync(int id)
 		{
-			var state = await _context.States.FirstOrDefaultAsync(x => x.Id == id);
+			var state = await _context.States
+				.Include(x => x.Cities)
+				.FirstOrDefaultAsync(x => x.Id == id);
 			if (state == null)
 			{
 				return NotFound();
