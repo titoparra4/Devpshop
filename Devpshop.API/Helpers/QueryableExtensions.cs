b9ab681a62476766a0b1a380275f52092f703261
Tito Parra <tito.parra4@hotmail.com>
2023-05-19 23:31:52 -0500

    Add Pagination Component

---
+++ Devpshop.API/Helpers/QueryableExtensions.cs
@@ -0,0 +1,16 @@
+﻿using Devpshop.Shared.DTOs;
+
+namespace Devpshop.API.Helpers
+{
+	public static class QueryableExtensions
+	{
+		public static IQueryable<T> Paginate<T>(this IQueryable<T> queryable,
+			PaginationDTO pagination)
+		{
+			return queryable
+				.Skip((pagination.Page - 1) * pagination.RecordsNumber)
+				.Take(pagination.RecordsNumber);
+		}
+	}
+
+}
